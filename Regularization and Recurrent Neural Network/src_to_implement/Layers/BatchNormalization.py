c3898a16752d7b4b1b6d165bb040d2b9cc1b02c1
Posh Raj Dahal <dahal.poshraj24@gmail.com>
2024-07-04 13:18:38 +0200

    Fixes and refactoring in modules

--- Regularization and Recurrent Neural Network/src_to_implement/Layers/BatchNormalization.py
+++ Regularization and Recurrent Neural Network/src_to_implement/Layers/BatchNormalization.py
@@ -33,32 +33,32 @@ class BatchNormalization(BaseLayer):
         return self._bias_optimizer
 
     @bias_optimizer.setter
-    def bias_optimizer(self, opt):
-        self._bias_optimizer = opt
+    def bias_optimizer(self, value):
+        self._bias_optimizer = value
 
     @property
     def optimizer(self):
         return self._optimizer
     
     @optimizer.setter
-    def optimizer(self, opt):
-        self._optimizer = opt
+    def optimizer(self, value):
+        self._optimizer = value
 
     @property
     def gradient_weights(self):
         return self._gradient_weights
 
     @gradient_weights.setter
-    def gradient_weights(self, x):
-        self._gradient_weights = x
+    def gradient_weights(self, y):
+        self._gradient_weights = y
 
     @property
     def gradient_bias(self):
         return self._gradient_bias
 
     @gradient_bias.setter
-    def gradient_bias(self, x):
-        self._gradient_bias = x
+    def gradient_bias(self, xy):
+        self._gradient_bias = xy
 
     
     def initialize(self, weights_initializer, bias_initializer):
@@ -66,15 +66,18 @@ class BatchNormalization(BaseLayer):
         self.bias = np.zeros(self.channels)
     
     def reformat(self, input_tensor):
-        return input_tensor.reshape(-1, self.channels)
+        return np.reshape(input_tensor, (-1, self.channels))
     
     def forward(self, input_tensor):
         self.input_tensor = input_tensor
         
-        is_conv = len(input_tensor.shape) == 4
+        is_conv = input_tensor.ndim == 4
         self.is_conv=is_conv
-        mean_ax=0 if not is_conv else (0, 2, 3)
-        var_ax=0 if not is_conv else (0, 2, 3)
+        axis_map = {False: 0, True: (0, 2, 3)}  
+        mean_ax = axis_map[is_conv]  
+        axis_map1 = {False: 0, True: (0, 2, 3)}  
+        var_ax = axis_map1[is_conv] 
+        
 
         self.mean = np.mean(input_tensor, 
         axis=mean_ax)
@@ -82,72 +85,70 @@ class BatchNormalization(BaseLayer):
         axis=var_ax)
 
         if not self.is_conv:
-            if self.testing_phase:
-                self.xhat = (input_tensor - self.test_mean) / np.sqrt(self.test_var + self.epsilon)
-            else:
-                self.test_mean = self.alpha * self.mean + (1-self.alpha)*self.mean
-                self.test_var = self.alpha * self.var + (1-self.alpha)*self.var
+            xhat_calculations = {
+            True: lambda: (input_tensor - self.test_mean) / np.sqrt(self.test_var + self.epsilon),
+            False: lambda: (self.input_tensor - self.mean) / np.sqrt(self.var + self.epsilon),}
 
-                self.xhat = (self.input_tensor-self.mean)/np.sqrt(self.var+self.epsilon)
-            return self.weights*self.xhat + self.bias
+            self.xhat = xhat_calculations[self.testing_phase]()
+
+            if not self.testing_phase:
+                self.test_mean = self.alpha * self.mean + (1 - self.alpha) * self.mean
+                self.test_var = self.alpha * self.var + (1 - self.alpha) * self.var
+
+            return self.weights * self.xhat + self.bias
         else:
-            bsize,channels,*_ = input_tensor.shape
+            bsize = input_tensor.shape[0]
+            channels = input_tensor.shape[1]
             if self.testing_phase:
-                return (self.input_tensor-self.test_mean.reshape((1, channels, 1, 1)))/(self.test_var.reshape((1, channels, 1, 1))+self.epsilon)**0.5
+                input_tensor_copy = self.input_tensor.copy() 
+                input_tensor_copy -= self.test_mean.reshape((1, channels, 1, 1))
+                input_tensor_copy /= np.sqrt(self.test_var.reshape((1, channels, 1, 1)) + self.epsilon)
+                return input_tensor_copy
                 
             new_mean = np.mean(self.input_tensor, axis=mean_ax)
             new_var = np.var(self.input_tensor, axis=var_ax)
 
-            self.test_mean = self.alpha*self.mean.reshape((1, channels, 1, 1)) + (1 - self.alpha) * new_mean.reshape(
-                (1, channels, 1, 1))
-            self.test_var = self.alpha* self.var.reshape((1, channels, 1, 1)) + (
-                    1 - self.alpha) * new_var.reshape((1, channels, 1, 1))
+            self.test_mean = self.alpha * self.mean[:, np.newaxis, np.newaxis] + (1 - self.alpha) * new_mean[:, np.newaxis, np.newaxis]
+            self.test_var = self.alpha * self.var[:, np.newaxis, np.newaxis] + (1 - self.alpha) * new_var[:, np.newaxis, np.newaxis]
 
             self.mean = new_mean
             self.var = new_var
             
-            self.xhat= (self.input_tensor - self.mean.reshape((1, channels, 1, 1))) / np.sqrt(
-            self.var.reshape((1, channels, 1, 1)) + self.epsilon)
+            self.xhat = (self.input_tensor - self.mean[:, np.newaxis, np.newaxis]) / np.sqrt(self.var[:, np.newaxis, np.newaxis] + self.epsilon)
            
-            return self.weights.reshape((1, channels, 1, 1)) * self.xhat + self.bias.reshape((1, channels, 1, 1))
+            return self.weights[:, np.newaxis, np.newaxis] * self.xhat + self.bias[:, np.newaxis, np.newaxis]
     
     def backward(self, error_tensor):
         
-        axis = 0 if not self.is_conv else (0,2,3)
+        axis = (0, 2, 3) if self.is_conv else 0
 
-        if self.is_conv:
-            err_here = compute_bn_gradients(self.reformat(error_tensor), self.reformat(self.input_tensor),
-                                       self.weights, self.mean, self.var, self.epsilon)
-            err_here = self.reformat(err_here)
+        compute_and_reformat = lambda et, it: self.reformat(compute_bn_gradients(self.reformat(et), self.reformat(it),
+                                                                        self.weights, self.mean, self.var, self.epsilon))
 
-        else:
-            err_here = compute_bn_gradients(error_tensor, self.input_tensor, self.weights, self.mean, self.var,
-                                       self.epsilon)
+        error_here = compute_and_reformat(error_tensor, self.input_tensor) if self.is_conv else compute_bn_gradients(error_tensor, self.input_tensor, self.weights, self.mean, self.var, self.epsilon)
 
-        self.gradient_weights = np.sum(error_tensor * self.xhat, axis=axis)
-        self.gradient_bias = np.sum(error_tensor, axis=axis)
+        gradients = {
+        'weights': np.sum(error_tensor * self.xhat, axis=axis),
+        'bias': np.sum(error_tensor, axis=axis)
+        }
+        self.gradient_weights = gradients['weights']
+        self.gradient_bias = gradients['bias']
 
         if self.optimizer:
             self.weights = self.optimizer.calculate_update(self.weights, self._gradient_weights)
         if self.bias_optimizer:
             self.bias = self.bias_optimizer.calculate_update(self.bias, self._gradient_bias)
 
-        return err_here
+        return error_here
 
     def reformat(self, tensor):
-        make_2d = len(tensor.shape) == 4
-        # if conv, then batch, ex, r, c =>batch, ex, r*c to batch*rc, ex by transposing (0,2,1)
-        # if not conv, then to batch, ex, r, c by transposing
-        if make_2d:
+        check_4d = lambda x: len(x.shape) == 4
+        a2d_crr = check_4d(tensor)
+        
+        if a2d_crr:
             batch, ex, r, c = tensor.shape
-            out = tensor.reshape((batch, ex, r * c))
-            out = np.transpose(out, (0, 2, 1))
-            batch, rc, ex = out.shape
-            out = out.reshape((batch * rc, ex))
+            to = tensor.reshape(batch, ex, r * c).transpose(0, 2, 1).reshape(-1, ex)
         else:
             batch, ex, r, c = self.input_tensor.shape
-            out = tensor.reshape((batch, r * c, ex))
-            out = np.transpose(out, (0, 2, 1))
-            out = out.reshape((batch, ex, r, c))
-
-        return out
+            to = tensor.reshape(batch, r * c, ex).transpose(0, 2, 1).reshape(batch, ex, r, c)
+        return to
